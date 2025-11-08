14556deb9f7d262c61dfbea2f3d40a870cd0347d
Awan Widodo <kurniawan.adi.widodo@gmail.com>
2025-11-08 10:27:32 +0700

    update tampilan dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -26,10 +26,10 @@
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-md-8">
-                                    <h4 class="mb-1">
+                                    <h5 class="mb-1">
                                         <i class="fas fa-hand-sparkles text-warning"></i> 
-                                        Selamat Datang, <?= $this->session->userdata('fullname') ?>!
-                                    </h4>
+                                        Selamat Datang, <?= $this->session->userdata('fullname') ?>
+                                    </h5>
                                     <p class="text-muted mb-0">
                                         <i class="fas fa-calendar-alt"></i> 
                                         <?= date('l, d F Y') ?> - 
