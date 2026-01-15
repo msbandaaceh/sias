304aa9e8d7132541744bd2b72c1e4c6adbae70f1
Awan Widodo <kurniawan.adi.widodo@gmail.com>
2026-01-15 08:37:11 +0700

    fix : perbaikan file config.php

--- application/config/config.php
+++ application/config/config.php
@@ -2,14 +2,24 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 date_default_timezone_set('Asia/Jakarta');
 
-$config['base_url'] = 'http://' .$_SERVER['SERVER_NAME'].'/';
-$config['sso_server'] = ''; # Domain SSO Server
-$config['id_app'] = ''; # ID Aplikasi Client
+$ssl =
+    (
+        (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ||
+        (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443) ||
+        (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
+    );
+
+$protocol = $ssl ? "https://" : "http://";
+
+$config['base_url'] = $protocol . $_SERVER['SERVER_NAME'] . '/';
+$config['sso_server'] = $protocol . 'sso.ms-bandaaceh.go.id/'; # Domain SSO Server
+$config['id_app'] = '3'; # ID Aplikasi Client
+$config['ptsp_server'] = $protocol . 'ptsp.ms-bandaaceh.go.id/'; # Domain PTSP Server
 
 $config['index_page'] = '';
-$config['uri_protocol']	= 'REQUEST_URI';
+$config['uri_protocol'] = 'REQUEST_URI';
 $config['url_suffix'] = '';
-$config['language']	= 'english';
+$config['language'] = 'english';
 $config['charset'] = 'UTF-8';
 $config['enable_hooks'] = FALSE;
 $config['subclass_prefix'] = 'MY_';
@@ -30,10 +40,10 @@ $config['log_date_format'] = 'Y-m-d H:i:s';
 $config['error_views_path'] = '';
 $config['cache_path'] = '';
 $config['cache_query_string'] = FALSE;
-$config['encryption_key'] = '';
+$config['encryption_key'] = '@M4hk4m4hBn4';
 
 $config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
+$config['sess_cookie_name'] = 'sso_msbna';
 $config['sess_samesite'] = 'Lax';
 $config['sess_expiration'] = 1800;
 $config['sess_save_path'] = NULL;
@@ -41,12 +51,12 @@ $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
 
-$config['cookie_prefix']	= '';
-$config['cookie_domain']	= '';
-$config['cookie_path']		= '/';
-$config['cookie_secure']	= FALSE;
-$config['cookie_httponly'] 	= FALSE;
-$config['cookie_samesite'] 	= 'Lax';
+$config['cookie_prefix'] = '';
+$config['cookie_domain'] = '.ms-bandaaceh.go.id';
+$config['cookie_path'] = '/';
+$config['cookie_secure'] = $ssl ? TRUE : FALSE;
+$config['cookie_httponly'] = TRUE;
+$config['cookie_samesite'] = 'Lax';
 
 $config['standardize_newlines'] = FALSE;
 $config['global_xss_filtering'] = FALSE;
@@ -63,6 +73,6 @@ $config['time_reference'] = 'local';
 $config['rewrite_short_tags'] = FALSE;
 $config['proxy_ips'] = '';
 
-$config['jwt_key'] = '';
-$config['jwt_issuer'] = '';
+$config['jwt_key'] = 'M4hk4m4hBn4@2025';
+$config['jwt_issuer'] = '.ms-bandaaceh.go.id';
 $config['jwt_expire_time'] = 3600; // 1 jam
